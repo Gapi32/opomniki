8043cf5a15e347240af1fa32351a2e4d342d2f05
Gašper K <gasper.kolbezen@gmail.com>
2016-03-08 11:40:41 +0000

    stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -19,7 +19,7 @@ window.addEventListener('load', function() {
 		document.querySelector("#cas_opomnika").value = "";
 		
 		document.querySelector("#opomniki").innerHTML += " \
-			<div class='opomnik'> \
+			<div class='opomnik senca rob'> \
             	<div class='naziv_opomnika'>" + naziv_opomnika + "</div> \
             	<div class='cas_opomnika'> Opomnik čez <span>" + cas_opomnika + 
             		"</span> sekund.</div> \
@@ -36,6 +36,15 @@ window.addEventListener('load', function() {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
+			
+			if (cas == 0) {
+				var naziv_opomnika = opomnik.querySelector(".naziv_opomnika").innerHTML;
+				alert("Opomnik!\n\nZadolžitev" + naziv_opomnika + "je potekla!")
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}
+			else {
+				casovnik.innerHTML = cas-1;
+			}
 	
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
